2b7f065f0cf4ed878e18d2db71159ba2ec3c983a
mohsinzafaruk1996 <mohsinzafaruk1996@gmail.com>
2020-06-19 15:02:59 +0100

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+from django.conf.urls import url
+from . import views
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
+
+
